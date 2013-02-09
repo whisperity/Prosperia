35f482aff9e5ffcdc8e96f70ae9b86b66a633935
Whisperity <whisperity@gmail.com>
2013-02-09 22:34:31 +0100

    UI changes in uploader

--- index.php
+++ index.php
@@ -17,13 +17,6 @@ use Prosperia\Stor\StorToFile as StorToFile;
     <body>
         
 <?php
-// DUMP
-var_dump($_POST);
-echo "<hr>";
-var_dump($_FILES);
-?>
-        
-        <?php
 function selfURL()
 {
 	/**
@@ -46,48 +39,63 @@ function selfURL()
 	return $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI'];
 }
 
-            if ( isset($_FILES['images']) )
+if ( isset($_FILES['images']) )
+{
+    echo "<span style=\"color: darkorange; font-weight: bold;\">Uploading " .count($_FILES['images']['name']).
+        " files.</span><br />";
+    for ($i = 0; $i < count($_FILES['images']['name']); $i++)
+    {
+        $allowed_types = array('image/jpeg', 'image/png', 'image/gif');
+        
+        if (in_array($_FILES['images']['type'][$i], $allowed_types, true))
+        {
+            $chars = "012345abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            $random = null;
+            
+            for ($j = 0; $j <= 8; $j++)
             {
-                for ($i = 0; $i < count($_FILES['images']['name']); $i++)
-                {
-                    
-                    $chars = "012345abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                    $random = null;
-                    
-                    for ($j = 0; $j <= 8; $j++)
-                    {
-                        $random .= $chars[rand(0, strlen($chars) - 1)];
-                    }
-                    
-                    $token = new ToknData($random, str_shuffle(sha1(time())));
-                    
-                    $upload_content = file_get_contents($_FILES['images']['tmp_name'][$i]);
-                    
-                    $stor = new Stor(new StorFromData(
-                        $_FILES['images']['name'][$i],
-                        $_FILES['images']['type'][$i],
-                        $_FILES['images']['size'][$i],
-                        $upload_content
-                    ));
-                    
-                    $writer = new StorToFile($stor, "var/stor/" . $token->getReference());
-                    
-                    $token->write();
-                    $writer->write();
-                    
-                    echo "File uploaded successfully. Token is: <strong>" . $token->getName() . "</strong>.<br>";
-                    
-                    $retrieve = str_replace(basename(__FILE__), "t/" . $token->getName(), selfURL());
-                    echo "Retrieve as: <a href=\"$retrieve\" target=\"_blank\">$retrieve</a>";
-                    
-                    exit;
-                }
+                $random .= $chars[rand(0, strlen($chars) - 1)];
             }
-        ?>
+            
+            $token = new ToknData($random, str_shuffle(sha1(time())));
+            
+            $upload_content = file_get_contents($_FILES['images']['tmp_name'][$i]);
+            
+            $stor = new Stor(new StorFromData(
+                $_FILES['images']['name'][$i],
+                $_FILES['images']['type'][$i],
+                $_FILES['images']['size'][$i],
+                $upload_content
+            ));
+            
+            $writer = new StorToFile($stor, "var/stor/" . $token->getReference());
+            
+            $token->write();
+            $writer->write();
+            
+            $retrieve = str_replace(basename(__FILE__), "t/" . $token->getName(), selfURL());
+            
+            echo "<span style=\"color: darkgreen; font-weight: bold;\">Successfully uplodaded <i>" .
+                $_FILES['images']['name'][$i]. "</i>.</span> Retrieve URL: <a href=\"$retrieve\" " .
+                "target=\"_blank\">$retrieve</a>.<br />";
+        }
+        else
+        {
+            echo "<span style=\"color: red; font-weight: bold;\">Won't upload <i>" .$_FILES['images']['name'][$i].
+                "</i>.</span> Type <i>" .$_FILES['images']['type'][$i]. "</i> is not allowed.<br />";
+        }
+    }
+}
+else
+{
+?>
         <form method="POST" action="index.php" enctype="multipart/form-data">
-            <label for="images[]">Images to upload:</label>
+            <label for="images[]">Images to upload: <small>(you can select multiple files)</small></label>
                 <input type="file" name="images[]" multiple=""/>
             <input type="submit" value="Upload"/>
         </form>
+<?php
+}
+?>
     </body>
 </html>
